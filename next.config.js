07b60232dac722056a1c2e7cb90a2fa024197fee
Kent Jordan <kentaki.dev@gmail.com>
2023-12-18 07:49:07 +0800

    Changed hostname

--- next.config.js
+++ next.config.js
@@ -5,9 +5,9 @@ const nextConfig = {
     remotePatterns: [{ hostname: "picsum.photos" }],
   },
   env: {
-    WS_GATEWAY_CHAT: "http://192.168.0.105:3500/chat",
-    WS_GATEWAY_INBOX: "http://192.168.0.105:3500/inbox",
-    API_HOSTNAME: "http://192.168.0.105:3001",
+    WS_GATEWAY_CHAT: "http://localhost:3500/chat",
+    WS_GATEWAY_INBOX: "http://localhost:3500/inbox",
+    API_HOSTNAME: "http://localhost:3001",
   },
 };
 

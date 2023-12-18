7c85acd76506cd4bd13584c542df6230dd7afe0b
Kent Jordan <kentaki.dev@gmail.com>
2023-12-18 18:09:37 +0800

    Added necessary variables for production and development

--- next.config.js
+++ next.config.js
@@ -1,13 +1,24 @@
 /** @type {import('next').NextConfig} */
+
+const PROD_API_PROTOCOL = "https";
+
+const PROD_API_HOSTNAME = "localhost";
+const PROD_WS_HOSTNAME = "localhost:3500";
+
+const DEV_API_PROTOCOL = "http";
+
+const DEV_API_HOSTNAME = "localhost:3001";
+const DEV_WS_HOSTNAME = "localhost:3500";
+
 const nextConfig = {
   reactStrictMode: false,
   images: {
     remotePatterns: [{ hostname: "picsum.photos" }],
   },
   env: {
-    WS_GATEWAY_CHAT: "http://localhost:3500/chat",
-    WS_GATEWAY_INBOX: "http://localhost:3500/inbox",
-    API_HOSTNAME: "http://localhost:3001",
+    WS_GATEWAY_CHAT: `${DEV_API_PROTOCOL}://${DEV_WS_HOSTNAME}/chat`,
+    WS_GATEWAY_INBOX: `${DEV_API_PROTOCOL}://${DEV_WS_HOSTNAME}/inbox`,
+    API_HOSTNAME: `${DEV_API_PROTOCOL}://${DEV_API_HOSTNAME}`,
   },
 };
 

aa69879f59d5193f73f67b6c09bbdeb71bc90e5d
Kent Jordan <kentaki.dev@gmail.com>
2024-04-12 14:50:20 +0800

    Added: new hostname

--- next.config.js
+++ next.config.js
@@ -24,7 +24,12 @@ const WS_HOSTNAME = setVariable({
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    remotePatterns: [{ hostname: REST_HOSTNAME }, { hostname: "localhost" }, { hostname: "cliemb.online" }],
+    remotePatterns: [
+      { hostname: REST_HOSTNAME },
+      { hostname: "localhost" },
+      { hostname: "cliemb.online" },
+      { hostname: "picsum.photos" },
+    ],
   },
   env: {
     WS_GATEWAY_CHAT: `${API_PROTOCOL}://${WS_HOSTNAME}/chat`,

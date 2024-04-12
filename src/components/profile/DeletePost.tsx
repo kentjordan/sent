8c454af55d02f58f7ef3ea2d1b332ec5c48d5731
Kent Jordan <kentaki.dev@gmail.com>
2024-04-12 18:56:53 +0800

    Refactored and added a spinner

--- src/components/profile/DeletePost.tsx
+++ src/components/profile/DeletePost.tsx
@@ -2,13 +2,40 @@ import useAppState from "@/hooks/useAppState";
 import { toggleDeletePost } from "@/redux/profile.slice";
 import { RootState } from "@/redux/store";
 import axios, { AxiosError } from "axios";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { PulseLoader } from "react-spinners";
 
 const DeletePost = () => {
   const dispatch = useDispatch();
 
-  const { id, content } = useSelector((state: RootState) => state.profile.activePost);
+  const { id } = useSelector((state: RootState) => state.profile.activePost);
   const { accessToken } = useAppState();
+
+  const [isPostDeleting, setIsPostDeleting] = useState(false);
+
+  const onDelete = () => {
+    const deleteUserPost = async () => {
+      try {
+        setIsPostDeleting(true);
+        const res = await axios.delete(`${process.env.API_HOSTNAME}/posts/${id}`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        if (res.status === 200) {
+          // Post deleted successfully
+          window.location.reload();
+        }
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          console.error(error.response);
+        }
+      }
+    };
+    deleteUserPost();
+  };
 
   return (
     <div className="absolute z-10 flex h-full w-full items-center justify-center bg-black/50">
@@ -16,31 +43,8 @@ const DeletePost = () => {
         <h1 className="text-xl font-bold">Delete post?</h1>
         <p className="my-2">This can’t be undone and it will be removed from your profile.</p>
         <div className="mt-8 flex flex-col gap-2">
-          <button
-            onClick={() => {
-              const deleteUserPost = async () => {
-                try {
-                  const res = await axios.delete(`${process.env.API_HOSTNAME}/posts/${id}`, {
-                    headers: {
-                      Authorization: `Bearer ${accessToken}`,
-                    },
-                  });
-
-                  if (res.status === 200) {
-                    // Post deleted successfully
-                    window.location.reload();
-                  }
-                } catch (error) {
-                  if (error instanceof AxiosError) {
-                    console.error(error.response);
-                  }
-                }
-              };
-              deleteUserPost();
-            }}
-            className="rounded-md bg-red-700 py-2 text-white"
-          >
-            Delete
+          <button onClick={onDelete} className="rounded-md bg-red-700 py-2 text-white">
+            {isPostDeleting ? <PulseLoader size={8} color="white" /> : "Delete"}
           </button>
           <button className="rounded-md border py-2" onClick={() => dispatch(toggleDeletePost(false))}>
             Cancel
@@ -52,4 +56,3 @@ const DeletePost = () => {
 };
 
 export default DeletePost;
-;
